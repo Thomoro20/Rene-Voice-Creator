0a66e59f3fb9f371cfffd4b08b040981572e1bd2
Thomoro20 <masertom71@gmail.com>
2025-07-17 11:01:05 +0200

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,21 @@
+
+import type { Phrase } from './types';
+
+export const PHRASES: Phrase[] = [
+  { id: 1, text: 'Ich habe Durst.', lang: 'de' },
+  { id: 2, text: 'Ich habe Hunger.', lang: 'de' },
+  { id: 3, text: 'Mir ist kalt.', lang: 'de' },
+  { id: 4, text: 'Mir ist warm.', lang: 'de' },
+  { id: 5, text: 'Kannst du mir bitte helfen?', lang: 'de' },
+  { id: 6, text: 'Mach bitte das Fenster auf.', lang: 'de' },
+  { id: 7, text: 'Mach bitte das Fenster zu.', lang: 'de' },
+  { id: 8, text: 'Schalte bitte das Licht ein.', lang: 'de' },
+  { id: 9, text: 'Schalte bitte das Licht aus.', lang: 'de' },
+  { id: 10, text: 'Wie geht es dir?', lang: 'de' },
+  { id: 11, text: 'Mir geht es gut, danke.', lang: 'de' },
+  { id: 12, text: 'Ich möchte gerne Radio hören.', lang: 'de' },
+  { id: 13, text: 'Danke vielmal.', lang: 'ch' }, 
+  { id: 14, text: 'Wie gaat’s?', lang: 'ch' },
+  { id: 15, text: 'Chönntsch mer öppis z trinke bringe?', lang: 'ch' },
+  { id: 16, text: 'Was git\'s hüt z\'Nacht?', lang: 'ch' },
+];
